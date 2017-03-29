c43476784d94ef30ba00916839bc74768be3b218
Rick921 <rickeando@gmail.com>
2017-03-29 11:19:38 +0200

    J=15
    Pequeño cambio para ver sincronización pull

--- src/repositoriogithub/RepositorioGitHub.java
+++ src/repositoriogithub/RepositorioGitHub.java
@@ -17,7 +17,7 @@ public class RepositorioGitHub {
     public static void main(String[] args) {
         // TODO code application logic here
         int i=0;
-        int j=0;
+        int j=15;
     }
     
 }
